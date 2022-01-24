c24a9aa71699423c20de550b2124d1c8f54fe4d0
Bruno Santos <bruno_st17@hotmail.com>
2022-01-23 21:08:07 -0300

    Refatoração de suite de test

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -1,8 +1,9 @@
 import signup from '../pages/SignupPage'
 import dataFactory from '../factories/dataFactory'
+import SignupPage from '../pages/SignupPage'
 
 describe('CADASTRO', () => {
-    
+
     it('Successful registration', function () {
         var deliver = dataFactory.deliver()
         const message = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
@@ -36,20 +37,26 @@ describe('CADASTRO', () => {
         signup.submit()
         signup.alertMessageShouldBe(message)
     })
-    
-    it('Required fields', function () {
 
-        signup.visitRegistrationPage()
-        signup.submit()
-        signup.alertMessageShouldBe('É necessário informar o nome')
-        signup.alertMessageShouldBe('É necessário informar o CPF')
-        signup.alertMessageShouldBe('É necessário informar o email')
-        signup.alertMessageShouldBe('É necessário informar o CEP')
-        signup.alertMessageShouldBe('É necessário informar o número do endereço')
-        signup.alertMessageShouldBe('Selecione o método de entrega')
-        signup.alertMessageShouldBe('Adicione uma foto da sua CNH')
-
-
+    context('Required fields', function () {
+        const messages = [
+            { field: 'nome', answer: 'É necessário informar o nome' },
+            { field: 'cpf', answer: 'É necessário informar o CPF' },
+            { field: 'email', answer: 'É necessário informar o email' },
+            { field: 'cep', answer: 'É necessário informar o CEP' },
+            { field: 'endereço', answer: 'É necessário informar o número do endereço' },
+            { field: 'metodoEntrega', answer: 'Selecione o método de entrega' },
+            { field: 'cnh', answer: 'Adicione uma foto da sua CNH' }
+        ]
+        before(function () {
+            signup.visitRegistrationPage()
+            signup.submit()
+        })
+        messages.forEach(function (m) {
+            it(`${m.field} é obrigatório`, function () {
+                SignupPage.alertMessageShouldBe(m.answer)
+            })
+        })
     })
 })
 
